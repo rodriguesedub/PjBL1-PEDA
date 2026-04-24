a8da257c1a7624947257f8bd94779a7111dbb2b7
rodriguesedub <rodrigues.epuc@gmail.com>
2026-04-23 23:53:52 -0300

    Update ProgramaFerrovia.java

--- ProgramaFerrovia.java
+++ ProgramaFerrovia.java
@@ -1,110 +1,84 @@
-import java.io.Serializable;
+import java.util.Scanner;
 
-// --- FRENTE 1: VAGÕES E PERSISTÊNCIA ---
-
-abstract class Vagao implements Serializable {
-    protected double comprimento; // [cite: 176]
-    protected double peso; // [cite: 177]
-    
-    public void imprime() {
-        System.out.println("Comprimento: " + comprimento + "m, Peso: " + peso + " toneladas."); // [cite: 181, 182]
-    }
-}
-
-class Locomotiva extends Vagao { // [cite: 186]
-    private double potencia; // [cite: 187]
-    
-    public Locomotiva(double comprimento, double peso, double potencia) {
-        this.comprimento = comprimento;
-        this.peso = peso;
-        this.potencia = potencia;
-    }
-    
-    @Override
-    public void imprime() { // [cite: 188, 189]
-        System.out.println("Tipo: Locomotiva. Potência: " + potencia + " HP."); // [cite: 191, 192]
-        super.imprime(); // [cite: 193]
-    }
-}
-
-class Passageiro extends Vagao {
-    private int numPassageiros;
-    
-    public Passageiro(double comprimento, double peso, int numPassageiros) {
-        this.comprimento = comprimento;
-        this.peso = peso;
-        this.numPassageiros = numPassageiros;
-    }
-    
-    @Override
-    public void imprime() {
-        System.out.println("Tipo: Passageiro. Lotação: " + numPassageiros + " passageiros.");
-        super.imprime(); // [cite: 124]
-    }
-}
-
-class Carga extends Vagao {
-    protected double carga; 
-    
-    public Carga(double comprimento, double peso) {
-        this.comprimento = comprimento;
-        this.peso = peso;
-        this.carga = peso * 0.75; // 75% do peso total é carga [cite: 100]
-    }
-    
-    @Override
-    public void imprime() {
-        System.out.println("Tipo: Carga. Carga suportada: " + carga + " toneladas.");
-        super.imprime(); // [cite: 124]
-    }
-}
-
-// A classe ObjetoPersistente já foi dada no PDF[cite: 19]. Basta copiá-la.
-
-// --- FRENTE 2: O MOTOR DO DEQUE ---
-
-class ComposicaoFerroviaria extends Deque implements Serializable { // [cite: 125]
-    ObjetoPersistente ArqComp; // [cite: 65]
-    
-    public ComposicaoFerroviaria(int N, String nomeArquivo) { // [cite: 66]
-        super(N); // [cite: 68]
-        ArqComp = new ObjetoPersistente(nomeArquivo); // [cite: 69]
-        carregar(); // [cite: 70]
-    }
-    
-    private void salvar() { ArqComp.salvar(this); } // [cite: 71]
-    private void carregar() { /* Implementação do PDF */ } // [cite: 73]
-    
-    public void criarComposicaoPadrao() { /* Lógica de limpar deque e adicionar 81 vagões */ salvar(); } // [cite: 108, 111, 198]
-    
-    public void inserirVagaoFrente(Vagao v) { /* Usa insertFront() do Deque */ salvar(); } // [cite: 112, 113]
-    public void inserirVagaoFim(Vagao v) { /* Usa insertLast() do Deque */ salvar(); } // [cite: 112, 113]
-    public Vagao removerVagaoFrente() { Vagao v = (Vagao) deleteFront(); salvar(); return v; } // [cite: 112, 113]
-    public Vagao removerVagaoFim() { Vagao v = (Vagao) deleteLast(); salvar(); return v; } // [cite: 112, 113]
-    
-    public void recuperarNumerosVagoes() { /* Conta total e subtipos */ } // [cite: 114]
-    public double contabilizarComprimento() { return 0.0; /* Lembrar do espaço de 2m */ } // 
-    public double contabilizarPesoTotal() { return 0.0; } // 
-    public void contabilizarPassageirosCarga() { /* Prints da soma */ } // [cite: 118]
-    public boolean verificarPotencia() { return false; /* HPT mínimo 1.05 */ } // [cite: 103, 119]
-    
-    public void diagnosticoComposicao() { // [cite: 121]
-        recuperarNumerosVagoes();
-        contabilizarComprimento();
-        contabilizarPassageirosCarga();
-        verificarPotencia();
-    }
-    
-    public Vagao getPrimeiroVagao() { return null; /* Fazer lógica */ } // [cite: 122]
-    public Vagao getUltimoVagao() { return null; /* Fazer lógica */ } // [cite: 122]
-}
-
-// --- FRENTE 3: INTERFACE E MENU ---
-
-public class ProgramaFerrovia { // [cite: 151]
+public class ProgramaFerrovia {
     public static void main(String[] args) {
-        // Criar o Scanner
-        // Instanciar ComposicaoFerroviaria
-        // Criar loop while com switch case para opções de 'a' até 'g' [cite: 130, 131, 132, 133, 134, 135, 136, 137]
+        Scanner sc = new Scanner(System.in);
+        ComposicaoFerroviaria comp = new ComposicaoFerroviaria(100, "composicao.dat");
+        
+        char opcao;
+        do {
+            System.out.println("=== MENU FERROVIÁRIO ===");
+            System.out.println("a) Criar Composição Padrão");
+            System.out.println("b) Inserir Vagão");
+            System.out.println("c) Remover Vagão");
+            System.out.println("d) Apresentar Diagnóstico");
+            System.out.println("e) Dados do Primeiro Vagão");
+            System.out.println("f) Dados do Último Vagão");
+            System.out.println("g) Terminar");
+            System.out.print("Escolha: ");
+            opcao = sc.next().toLowerCase().charAt(0);
+
+            switch (opcao) {
+                case 'a':
+                    comp.criarComposicaoPadrao();
+                    break;
+                case 'b':
+                    if (comp.isFull()) {
+                        System.out.println("Erro: Deque cheio!");
+                        break;
+                    }
+                    System.out.println("Tipo: 1-Locomotiva, 2-Passageiro, 3-Carga");
+                    int tipo = sc.nextInt();
+                    System.out.print("Comprimento (m): ");
+                    double c = sc.nextDouble();
+                    System.out.print("Peso (t): ");
+                    double p = sc.nextDouble();
+                    
+                    Vagao novo = null;
+                    if (tipo == 1) {
+                        System.out.print("Potência (HP): ");
+                        novo = new Locomotiva(c, p, sc.nextDouble());
+                    } else if (tipo == 2) {
+                        System.out.print("Qtd Passageiros: ");
+                        novo = new Passageiro(c, p, sc.nextInt());
+                    } else {
+                        novo = new Carga(c, p);
+                    }
+                    
+                    System.out.print("Onde inserir? (1-Início, 2-Fim): ");
+                    if (sc.nextInt() == 1) comp.adicionarVagaoInicio(novo);
+                    else comp.adicionarVagaoFim(novo);
+                    break;
+                    
+                case 'c':
+                    if (comp.isEmpty()) {
+                        System.out.println("Erro: Deque vazio!");
+                    } else {
+                        System.out.print("Remover de onde? (1-Início, 2-Fim): ");
+                        Vagao removido = (sc.nextInt() == 1) ? comp.removerVagaoInicio() : comp.removerVagaoFim();
+                        System.out.print("Removido: ");
+                        removido.imprime();
+                    }
+                    break;
+                    
+                case 'd':
+                    comp.mostrarDiagnostico();
+                    break;
+                    
+                case 'e':
+                    Vagao prim = comp.getPrimeiro();
+                    if (prim != null) prim.imprime();
+                    else System.out.println("Vazio.");
+                    break;
+                    
+                case 'f':
+                    Vagao ult = comp.getUltimo();
+                    if (ult != null) ult.imprime();
+                    else System.out.println("Vazio.");
+                    break;
+            }
+        } while (opcao != 'g');
+        
+        sc.close();
     }
 }
